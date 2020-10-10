2013cefd24a93cc594d3164b81b38cad9e2fdeb7
RAJKUM-BLRM20 <raj.kumar@rajkum-blrm20.dlink>
2020-10-10 15:31:42 +0530

    CI/CD

---
+++ travis-ci-python-demo-master/README.md
@@ -0,0 +1,2 @@
+# travis-ci-python-demo
+Demonstrates some basic examples of how Travis CI can be included in your Python CI/CD pipeline.
---
+++ travis-ci-python-demo-master/hello-world-requirements.py
@@ -0,0 +1,5 @@
+# import pyfiglet module 
+from pyfiglet import Figlet
+
+f = Figlet(font='isometric1')
+print(f.renderText('Hello World')) 
